da65d665ef083f5042e35936e1e612d1fac1b6c9
Ethan Brews <ethan.brews@hpe.com>
2023-12-08 16:46:39 +0000

    Finish day 3

--- AdventOfCode/Days/Day03.cs
+++ AdventOfCode/Days/Day03.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode.Days;
+﻿using System.Collections;
+
+namespace AdventOfCode.Days;
 
 [ChallengeName("Gear Ratios")]
 public class Day03 : IDay
@@ -9,45 +11,63 @@ public class Day03 : IDay
         var symbols = lines
             .SelectMany((line, i) => line.Select((c, j) => (c, i, j)))
             .Where(o => "%+-$=@#*/&".Contains(o.c))
-            .Select(o => (o.i, o.j))
-            .ToList();
-
-        var total = 0;
-        foreach (var s in symbols)
-        {
-            for (var c = 0; c < 9; c++)
-            {
-                var i = (c / 3)-1;
-                var j = (c % 3)-1;
-                if (s.i + i < 0 || s.i + i >= lines.Length || s.j + j < 0 || s.j + j >= lines[s.i + i].Length)
-                    continue;
-                if (!char.IsDigit(lines[s.i + i][s.j + j])) continue;
-                total += ReadIntAtPosition(lines[s.i + i], s.j + j);
-                break;
-            }
-        }
-
-        return total;
+            .Select(o => (o.i, o.j));
+        return symbols
+            .Select(s => SearchForSurroundingNumbers(s, lines))
+            .SelectMany(ns => ns
+                .Select(n => SearchForFirstDigit(n, lines))
+                .Distinct())
+            .Select(s => ReadIntAtPosition(lines[s.i], s.j))
+            .Sum();
     }
 
-    private static int ReadIntAtPosition(string line, int digitPosition)
+    private static (int i, int j) SearchForFirstDigit((int i, int j) s, IReadOnlyList<string> lines)
     {
-        var cursor = digitPosition;
-        while (cursor > 0 && char.IsDigit(line[cursor - 1]))
+        var cursor = s.j;
+        while (cursor > 0 && char.IsDigit(lines[s.i][cursor - 1]))
         {
             cursor--;
         }
 
-        var length = 0;
-        while (char.IsDigit(line[cursor + (length++) + 1]))
+        return (s.i, cursor);
+    } 
+
+    private static IEnumerable<(int, int)> SearchForSurroundingNumbers((int i, int j) s, IReadOnlyList<string> lines)
+    {
+        for (var c = 0; c < 9; c++)
         {
+            var i = (c / 3)-1;
+            var j = (c % 3)-1;
+            if (s.i + i < 0 || s.i + i >= lines.Count || s.j + j < 0 || s.j + j >= lines[s.i + i].Length)
+                continue;
+            if (!char.IsDigit(lines[s.i + i][s.j + j])) continue;
+            yield return (s.i + i, s.j + j);
         }
-        //Console.WriteLine($"{line[..cursor]}[{line[cursor..(cursor + length)]}]{line[(cursor+length)..]}");
-        return Convert.ToInt32(line[cursor..(cursor + length)]);
+    }
+
+    private static int ReadIntAtPosition(string line, int digitPosition) 
+    {
+        var length = 0;
+        while (char.IsDigit(line[digitPosition + (length++) + 1])) { }
+        return Convert.ToInt32(line[digitPosition..(digitPosition + length)]);
     }
 
     public object SolvePartTwo(string input)
     {
-        throw new NotImplementedException();
+        var lines = input.Split('\n');
+        var symbols = lines
+            .SelectMany((line, i) => line.Select((c, j) => (c, i, j)))
+            .Where(o => "%+-$=@#*/&".Contains(o.c))
+            .Select(o => (o.i, o.j));
+        return symbols
+            .Select(s => SearchForSurroundingNumbers(s, lines))
+            .Select(ns => ns
+                .Select(n => SearchForFirstDigit(n, lines))
+                .Distinct())
+            .Where(ns => ns.Count() == 2)
+            .Select(ss => ss
+                .Select(s => ReadIntAtPosition(lines[s.i], s.j))
+                .Aggregate(1, (acc, val) => acc * val))
+            .Sum();
     }
 }
